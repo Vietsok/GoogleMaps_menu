d2c085b6edeac8416d02bd591a54f942b5394b2d
VietSokNguyen <vietsok@gmail.com>
2022-01-04 16:05:53 +0000

    Bottom v 1.0

---
+++ app/src/main/java/com/sharkilver/googlemaps/ui/home/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.sharkilver.googlemaps.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import com.sharkilver.googlemaps.R;
+import com.sharkilver.googlemaps.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+    private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
+
